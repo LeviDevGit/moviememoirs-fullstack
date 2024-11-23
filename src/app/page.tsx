d3f50974da9b7813209875d6e3c9404d18d89e2a
Levi <114665569+LeviDevGit@users.noreply.github.com>
2024-11-23 17:38:03 -0300

    :construction: feat: Formação da página e componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import MovieCard from '@/components/MovieCard'
+import SearchInput from '@/components/SearchInput'
+
 export default function Home() {
-  return <div className="h-full"></div>
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-between p-10">
+      <header className="flex w-full justify-between border border-red-500">
+        <SearchInput />
+        <div className="flex gap-4 text-white">
+          <button>Adicionar</button>
+          <button>Gerenciar</button>
+        </div>
+      </header>
+      <main className="w-full border border-red-500">
+        <div className="flex w-full items-center justify-center">
+          <MovieCard source={{ value: 5 }} />
+        </div>
+      </main>
+    </div>
+  )
 }
---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,57 @@
+import { Star, StarHalf } from 'lucide-react'
+import Image from 'next/image'
+
+interface MovieCardProps {
+  source: {
+    value: number
+  }
+}
+
+function renderStars(value: number) {
+  const starColor = '#841B2D'
+  const fullStar = Math.floor(value)
+  const stars = []
+
+  for (let i = 0; i < fullStar; i++) {
+    stars.push(<Star fill={starColor} color={starColor} size={40} />)
+  }
+
+  if (value - fullStar) {
+    stars.push(<StarHalf fill={starColor} color={starColor} size={40} />)
+  }
+
+  return stars.map((element, index) => element.key === `${index}`)
+}
+
+export default function MovieCard({ source }: MovieCardProps) {
+  return (
+    <div className="flex h-[696px] w-fit flex-col items-center rounded-md text-white">
+      <div className="group relative w-fit">
+        <div className="pointer-events-none absolute left-1/2 top-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 opacity-0 blur duration-[1000ms] group-hover:opacity-100 group-hover:blur-none">
+          {renderStars(source.value)}
+        </div>
+        <div className="shadow-cardShadow w-[345px] object-cover object-center duration-[2500ms] hover:blur hover:grayscale">
+          <Image
+            alt="Poster"
+            src="https://image.tmdb.org/t/p/original/bazlgJAKWCBF7OllC3JtyJ2DV1n.jpg"
+            width={345}
+            height={518}
+          />
+        </div>
+      </div>
+      <div className="w-[345px] pt-4">
+        <div className="h-[80px] overflow-hidden">
+          <h2 className="text-ghost-white text-center text-2xl font-semibold">
+            O Enigma de Outro Mundo
+          </h2>
+        </div>
+        <hr className="w-full border border-[#ffffff4d]" />
+        <div className="text-silver mt-3 flex flex-col gap-1 font-['Inter'] text-sm">
+          <span>John Carpenter</span>
+          <br />
+          <span className="self-end">1hr 49min | 1982</span>
+        </div>
+      </div>
+    </div>
+  )
+}
