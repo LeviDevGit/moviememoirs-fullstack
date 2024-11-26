af38ae2366d5df6247f8b568c8c39cb5f2225f44
Levi <114665569+LeviDevGit@users.noreply.github.com>
2024-11-25 21:13:29 -0300

    :sparkles: feat: Prisma setup sqlite database

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -11,7 +11,7 @@ export default function NavigationButton({
 }: NavigationButtonProps) {
   return (
     <button
-      className={`group absolute inset-y-0 z-10 flex w-fit items-center justify-start from-zinc-900 to-transparent ${left ? 'left-0 bg-gradient-to-r' : 'right-0 bg-gradient-to-l'}`}
+      className={`group absolute inset-y-0 z-10 flex w-[100px] items-center from-zinc-900 to-transparent ${left ? 'left-0 justify-start bg-gradient-to-r' : 'right-0 justify-end bg-gradient-to-l'}`}
     >
       {children}
     </button>
